445745bc390b871fea9cb513a581c500edbbbb3d
divyansh357 <divyanshkumargupta532@gmail.com>
2024-11-09 15:43:22 +0530

    Add files via upload

---
+++ operators.java
@@ -0,0 +1,116 @@
+//import java.util.*;
+public class operators {
+    public static void main(String arg[])
+    {
+        //Binary Operators
+        // int A = 10;
+        // int B = 5;
+        // System.out.println("Add="+ (A+B));
+        // System.out.println("Subtract="+(A-B));
+        // System.out.println("Multiply="+(A*B));
+        // System.out.println("Divide="+(A/B));
+        // System.out.println("Modulo(remainder)="+(A% B));
+
+        //Unary Operators
+        //Pre Increment
+        // int a = 10;
+        // int b = ++a; // first increment is done and then value is assigned
+        // System.out.println(a);
+        // System.out.println(b);
+
+        //Post Increment
+        // int c = 10;
+        // int d = c++; // first value is assigned to d and then increment is done
+        // System.out.println(c);
+        // System.out.println(d);   
+
+        // Pre Decrement 
+        // int a = 10;
+        // int b = --a;
+        // System.out.println(a);
+        // System.out.println(b);
+         
+        // Post Decrement 
+        // int c = 10;
+        // int d = c--;
+        // System.out.println(c);
+        // System.out.println(d);
+
+        //Relational Operators
+
+        // int a = 10;
+        // int b = 5;
+        // System.out.println(a==b);
+        // System.out.println(a!=b);
+        // System.out.println(a>b);
+        // System.out.println(a<b);
+        // System.out.println(a>=b);
+        // System.out.println(a<=b);
+
+        // Logical Operators
+        // System.out.println((3>2) && (5>0)); // Logical and 
+        // System.out.println( (3<2) ||(5<0) ); // Logical or
+        // System.out.println(!(0<5));
+
+        // Assignment Operators
+        // int a = 10;
+        // a +=10; // a = a+10
+        // System.out.println(a);
+        // a-=5; // a=a-5
+        // System.out.println(a);
+        // a*=2; // a=a*2
+        // System.out.println(a);
+        // a/=5; // a=a/5
+        // System.out.println(a);
+        // a%=3; // a=a%3
+        // System.out.println(a);
+
+        // Questions
+        //q1
+        // int x=2, y=5;
+        // int exp1 = (x*y/x);
+        // int exp2 = (x*(y/x));
+        // System.out.println(exp1);
+        // System.out.println(exp2);
+
+        //q2
+        // int x = 200, y=50, z=100;
+        // if(x>y && y>z)
+        // {
+        //     System.out.println("Hello");
+        // }
+        // if(z>y && z<x)
+        // {
+        //     System.out.println("Java");
+        // }
+        // if((y+200)<x && (y+150)<z)
+        // {
+        //     System.out.println("Hello Java");
+        // }
+
+        //q3
+
+        // int x,y,z;
+        // x=y=z=2;
+        // x+=y;
+        // y-=z;
+        // z/=(x+y);
+        // System.out.println(x+""+y+""+z);
+
+        //q4
+        // int x = 9, y=12;
+        // int a=2, b=4,c=6;
+        // int exp = 4/3 * (x+34)+ 9 * (a+b*c) + (3+ y*(2+a)) /(a+b*y);
+        // System.out.println(exp);
+
+        //q5
+        // int x =10, y=5;
+        // int exp1 =(y*(x/y+x/y));
+        // int exp2 =(y*x/y+y*x/y);
+        // System.out.println(exp1);
+        // System.out.println(exp2);
+
+    }
+    
+}
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
