5f531f47ce5366049011324385fafc1336899e2e
divyansh357 <divyanshkumargupta532@gmail.com>
2024-11-09 15:41:13 +0530

    Create Basics2.java

---
+++ Basics2.java
@@ -0,0 +1,59 @@
+// Inputs In Java
+//import java.util.*;
+
+public class basics2 {
+    public static void main(String args[])
+    {
+      // Scanner s = new Scanner(System.in);
+        // String input = s.next(); //This captures only single word
+        // System.out.println(input);
+
+        // String name = s.nextLine(); // Both spaces and words are captured, Multi word input
+        // System.out.println(name);
+
+        // int num = s.nextInt(); // Int inut
+        // System.out.println(num);
+
+        // float price = s.nextFloat();
+        // System.out.println(price);
+
+        // boolean ch = s.nextBoolean();
+        // System.out.println(ch);
+
+        // short t = s.nextShort();
+        // System.out.println(t);
+
+        // long w = s.nextLong();
+        // System.out.println(w);
+
+        // Sum of a and b
+
+        // Scanner t = new Scanner(System.in);
+
+        // int a = t.nextInt();
+        // int b = t.nextInt();
+        // int sum = a+b;
+        // System.out.println(sum);
+
+        // Product Of a and b
+
+        // Scanner q = new Scanner(System.in);
+
+        // int a = q.nextInt();
+        // int b = q.nextInt();
+        // int product = a*b;
+        // System.out.println(product);
+
+        // Area Of Circle 
+        
+        // Scanner o = new Scanner(System.in);
+        // float r = o.nextFloat();
+        // float area = 3.14f*r*r; // To consider 3.14 as float suffix f is added
+        // System.out.println(area);
+        
+    }
+
+}  
+
+    
+
