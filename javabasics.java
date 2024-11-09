1edf84df6ec40247a6bc69fb8ef37b28e2642a9c
divyansh357 <divyanshkumargupta532@gmail.com>
2024-11-09 15:32:26 +0530

    Add files via upload

---
+++ javabasics.java
@@ -0,0 +1,67 @@
+public class javabasics {
+    public static void main(String arg[]){
+        // System.out.print("Hello Divyansh\n"); // \n is used for next line
+        // System.out.println("Hello Divyansh");//ln is used for next line
+        // System.out.println("Hello Divyansh");
+
+        //Print Pattern
+
+        // System.out.println("****");
+        // System.out.println("***");
+        // System.out.println("**");
+        // System.out.println("*");
+
+        // Variables in Java
+        // int a=10;
+        // int b=5;
+        // System.out.println(a);
+        // System.out.println(b);
+        // String name="Pablo Escobar";
+        // System.out.println(name);
+
+
+        // a=b;
+        // System.out.println(a);
+
+        //Datatype 
+        // byte b=8;
+        // System.out.println(b);
+
+        // char ch='a';
+        // System.out.println(ch);
+
+        // boolean var=false;
+        // System.out.println(var);
+
+        // long d=823134567;
+        // System.out.println(d);
+
+        // float price= 1.2f; // if we dont append with f it will be saved as double 
+        // System.out.println(price);
+
+        // double w=45676544435.24852;
+        // System.out.println(w);
+
+        // short n=240;
+        // System.out.println(n);
+
+        // Sum of a and b
+
+        // int a=10;
+        // int b=5;
+        // int sum=a+b;
+        // System.out.println(sum);
+
+        //Comments In Java
+
+        //Single line comments
+
+        /*
+         Multi line comments 
+         hello
+        */
+
+    }
+}
+//boilerplate code
+
