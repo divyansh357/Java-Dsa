1475004e597e565396a95b1d0fbd9c952e9beb4c
divyansh357 <divyanshkumargupta532@gmail.com>
2024-12-03 09:03:54 +0530

    Update Array.java

--- Array.java
+++ Array.java
@@ -230,7 +230,255 @@ public class Array
         System.out.println("Our maximum subarray sum is:"+ms);
         }
     }
-    public  static void main(String args[])
+
+    // Trapping Rainwater
+    public static int trappedRainwater(int height[])
+    {
+        int n = height.length;
+        //calculate left max boundary -array
+        int leftMax []= new int[n];
+        leftMax[0]=height[0];
+        for(int i=1;i<n;i++)
+        {
+            leftMax[i]= Math.max(height[i],leftMax[i-1]);
+        }
+        //calculate right max bounady - array
+        int rightmax [] = new int[n];
+        rightmax[n-1]= height[n-1];
+        for(int i=n-2;i>=0;i--)
+        {
+            rightmax[i]=Math.max(height[i],rightmax[i+1]);
+        }
+
+        int trappedWater = 0;
+        // loop 
+        for(int i=0;i<n;i++)
+        {
+            // waterLevel = min(leftmax bound,rightmax bound)
+            int waterLevel = Math.min(leftMax[i],rightmax[i]);
+            // tarpped water = waterlevel - height[i]
+            trappedWater+= waterLevel -height[i];
+        }
+
+        return trappedWater; 
+    }
+    // Buy and Sell Stocks
+    public static int buy_sell_stock(int prices[])
+    {
+        int buy_price = Integer.MAX_VALUE;
+        int max_profit = 0;
+        for(int i=0;i<prices.length;i++)
+        {
+            if(buy_price<prices[i])
+            {
+                int profit = prices[i]-buy_price;
+                max_profit = Math.max(profit,max_profit);
+            }
+            else
+            {
+                buy_price = prices[i];
+            }
+        }
+        return max_profit;
+    }
+    //q1
+    public static boolean repeat(int nums[])
+    {
+        boolean isRepeat = false;
+        for(int i=0;i<nums.length;i++)
+        {
+            for(int j=i+1;j<nums.length;j++)
+            {
+                if(nums[i]==nums[j])
+                {
+                    isRepeat = true;
+                }
+            }
+        }
+        return isRepeat;
+    }
+    //q2
+    public static int search(int prices[],int key)
+    {
+        int st=0,end=prices.length-1;
+        while(st<=end)
+        {
+            int mid = st + (end-st)/2;
+            if(prices[mid]==key) // left sorted
+            {
+                return mid;
+            }
+            if(prices[st]<=prices[mid])
+            {
+                if(prices[st]<=key && key<=prices[mid])
+                {
+                    end = mid-1;
+                } 
+                else
+                {
+                    st = mid+1;
+                }
+            }
+            else  // right sorted
+            {
+                if(prices[mid]<=key && key<=prices[end])
+                {
+                    st = mid+1;
+                }
+                else
+                {
+                    end = mid-1;
+                }
+            }
+        }
+        return -1;
+    }
+    //q2
+    // public static int search(int nums[],int target)
+    // {
+    //     // min will have the index of minimum element of nums
+    //     int min = minSearch(nums);
+    //     // find in sorted left 
+    //     if(nums[min]<=target && target<=nums[nums.length-1])
+    //     {
+    //         return search(nums,min,nums.length-1,target);
+    //     }
+    //     //find in sorted right 
+    //     else
+    //     {
+    //         return search(nums,0,min,target);
+    //     }
+
+    // }
+    // // Binary serach to find 
+    // public static int search(int nums[],int left,int right,int target)
+    // {
+    //     int l = left;
+    //     int r = right;
+    //     //System.out.println(left+" "+right);
+    //     while(l<=r)
+    //     {
+    //         int mid = l + (r-1)/2;
+    //         if(nums[mid]==target)
+    //         {
+    //             return mid;
+    //         }
+    //         else if(nums[mid]>target)
+    //         {
+    //             r = mid-1;
+    //         }
+    //         else
+    //         {
+    //             l=mid+1;
+    //         }
+    //     }
+    //     return -1;
+    // }
+    // public static int minSearch(int nums[])
+    // {
+    //     int left =0;
+    //     int right = nums.length -1;
+    //     while(left<right)
+    //     {
+    //         int mid = left + (right-left)/2;
+    //         if(mid>0 && nums[mid-1]>nums[mid])
+    //         {
+    //             return mid;
+    //         }
+    //         else if(nums[left]<=nums[mid] && nums[mid]>nums[right])
+    //         {
+    //             left = mid+1;
+    //         }
+    //         else
+    //         {
+    //             right = mid-1;
+    //         }
+    //     }
+    //     return left;
+    // }
+    // public static int max_profit(int prices[])
+    // {
+    //     for(int i=0;i<)
+    // }
+
+    // q3
+    public static int profit(int prices[])
+    {
+        int max_profit = 0;
+        int buy_price = Integer.MAX_VALUE;//tarck the lowest buying price
+        int curr_profit=0;
+        for(int i=0;i<prices.length;i++)
+        {
+            if(buy_price<prices[i])
+            {
+                curr_profit = prices[i]-buy_price;
+                max_profit = Math.max(max_profit, curr_profit);
+            }
+            else
+            {
+                buy_price = prices[i];
+            }
+        }
+        return max_profit;
+    }
+    //q4
+
+    public static int rain(int height[])
+    {
+        int n = height.length;
+
+        // Calculate leftmax boundary
+        int leftMax[] = new int[n];
+        leftMax[0] = height[0];
+        for(int i=1;i<n;i++)
+        {
+            leftMax[i] = Math.max(height[i],leftMax[i-1]);
+        }
+
+        // Calculate rightmax boundary
+        int rightmax[] = new int[n];
+        rightmax[n-1] = height[n-1];
+        for(int i=n-2;i>=0;i--)
+        {
+            rightmax[i] = Math.max(height[i],rightmax[i+1]);
+        }
+
+        int trappedRainwater =0;
+        for(int i=0;i<n;i++)
+        {
+            int waterLevel = Math.min(leftMax[i],rightmax[i]);
+            trappedRainwater+= waterLevel - height[i];
+        }
+        return trappedRainwater;
+    }
+
+    //q5
+    public List<List<Integer>> threeSum(int nums[])
+    {
+        List<List<Integer>> result = new ArrayList <List<Integer>> ();
+
+        for(int i=0;i<nums.length;i++)
+        {
+            for(int j=i+1;j<nums.length;j++)
+            {
+                for(int k=j+1;j<nums.length;k++)
+                {
+                    if(nums[i]+nums[j]+nums[k]==0)
+                    {
+                        List<Integer> triplet = new ArrayList <Integer>();
+                        triplet.add(nums[i]);
+                        triplet.add(nums[j]);
+                        triplet.add(nums[k]);
+                        Collections.sort(triplet);
+                        result.add(triplet);
+                    }
+                }
+            }
+        }
+        result = new ArrayList<List<Integer>>(new LinkedHashSet<List<Integer>>(result));
+        return result;
+    }
+    public static void main(String args[])
     {
         // CReating an array
         // int marks[] = new int[100];
@@ -349,6 +597,39 @@ public class Array
         //Kadane_2(numbers2);
 
         // Trapping Rainwater
+        // int height[] = {4,2,0,6,3,2,5};
+        // int trappedWater=trappedRainwater(height);
+        // System.out.println("Trapped Rainwater = "+trappedWater);
         
-    }
+        //Buy and Sell Stocks
+        // int prices[] = {7,1,5,3,6,4};
+        // System.out.println(buy_sell_stock(prices));
+
+        // Assignment 
+
+        //q1
+        // int nums[] = {1,1,1,3,3,4,3,2,4,2};
+        // System.out.println(repeat(nums));
+
+        //q2
+        // int prices[] = {6,7,8,0,1,2,3,4,5};
+        // int key = 1;
+        // System.out.println(search(prices,key));
+
+        //q3
+        // int prices[] = {7,1,5,3,6,4};
+        // System.out.println((profit(prices)));
+
+        //q4
+        // int height[] = {4,2,0,3,2,5};
+        // System.out.println("Trapped Rainwater= "+rain(height));
+
+        // q5=threeSum
+        Array example = new Array();
+        int nums[]= {-1,0,1,2-1,-4};
+
+        List<List<Integer>> result = example.threeSum(nums);
+
+        System.out.println("Triplets of sum 0: "+ result);
+    }        
 }
