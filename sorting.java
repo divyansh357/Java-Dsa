feeab939b879028f4b67216778db6f98b641830d
divyansh357 <divyanshkumargupta532@gmail.com>
2024-12-04 22:02:47 +0530

    Update sorting.java

--- sorting.java
+++ sorting.java
@@ -173,7 +173,30 @@ public class sorting
     }
     public static void counting(int arr[])
     {
-        
+        int largest = Integer.MIN_VALUE;
+        //finding range
+        for(int i=0;i<arr.length;i++)
+        {
+            largest = Math.max(largest,arr[i]);
+        }
+        //creating count
+        int count[] = new int[largest+1];
+        for(int i=0;i<arr.length;i++)
+        {
+            count[arr[i]]++;
+        }
+        //Sorting
+        int j=0;
+        for(int i=0;i<count.length;i++)
+        {
+            while(count[i]>0)
+            {
+                arr[j]=i;
+                j++;
+                count[i]--;
+            }
+        }
+
     }
     public static void main(String args[])
     {
