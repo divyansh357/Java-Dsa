fc3755812de130d99d5258649d05ddf7e15caaac
Divyansh Kumar Gupta <divyanshkumargupta532@gmail.com>
2026-05-11 21:47:43 +0530

    Array List Traversing

--- ArrayList/ArrayList1.java
+++ ArrayList/ArrayList1.java
@@ -76,28 +76,54 @@ public class ArrayList1 {
         // System.out.println(list);
 
         // 2-D Array 
-        ArrayList<ArrayList<Integer>> mainList = new ArrayList<>();
+        //ArrayList<ArrayList<Integer>> mainList = new ArrayList<>();
 
-        ArrayList<Integer> list1 = new ArrayList<>();
-        list1.add(1);
-        list1.add(2);
+        // ArrayList<Integer> list1 = new ArrayList<>();
+        // list1.add(1);
+        // list1.add(2);
 
-        mainList.add(list1);
+        // mainList.add(list1);
 
-        ArrayList<Integer> list2 = new ArrayList<>();
-        list2.add(3);
-        list2.add(4);
+        // ArrayList<Integer> list2 = new ArrayList<>();
+        // list2.add(3);
+        // list2.add(4);
 
-        mainList.add(list2);
+        // mainList.add(list2);
 
-        for(int i=0;i<mainList.size();i++){
-            ArrayList<Integer> arr = mainList.get(i);
-            for(int j=0;j<arr.size();j++){
-                System.out.print(arr.get(j)+" ");
-            }
-            System.out.println();
-        }
-        System.out.println(mainList);
+        // for(int i=0;i<mainList.size();i++){
+        //     ArrayList<Integer> arr = mainList.get(i);
+        //     for(int j=0;j<arr.size();j++){
+        //         System.out.print(arr.get(j)+" ");
+        //     }
+        //     System.out.println();
+        // }
+        // System.out.println(mainList);
+
+        // ArrayList<Integer> list1 = new ArrayList<>();
+        // ArrayList<Integer> list2 = new ArrayList<>();
+        // ArrayList<Integer> list3 = new ArrayList<>();
+
+        // for(int i=1;i<=5;i++){
+        //     list1.add(i);
+        //     list2.add(i*2);
+        //     list3.add(i*3);
+        // }
+
+        // mainList.add(list1);
+        // mainList.add(list2);
+        // mainList.add(list3);
+
+        // for(int i=0;i<mainList.size();i++){
+        //     ArrayList<Integer> list = mainList.get(i);
+        //     for(int j=0;j<list.size();j++){
+        //         System.out.print(list.get(j)+" ");
+        //     }
+        //     System.out.println();
+        // }
+        // System.out.println();
+
+        //
+
 
     }
 
